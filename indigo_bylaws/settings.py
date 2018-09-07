ce68fc7bef608a08915352866e920d3f078b3b80
Greg Kempe <greg@kempe.net>
2018-09-07 11:36:58 +0200

    Integrate with elk

--- indigo_bylaws/settings.py
+++ indigo_bylaws/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from indigo.settings import *
 
 
@@ -6,12 +8,41 @@ INSTALLED_APPS = (
 ) + INSTALLED_APPS + (
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
+    'elasticapm.contrib.django',
 )
+
+# Elasticsearch and logstash
+LOGSTASH_URL = os.environ.get('LOGSTASH_URL', '')
+APM_SERVER_URL = os.environ.get('APM_SERVER_URL', '')
+ELK_PROJECT = 'indigo-bylaws'
+ELASTIC_APM = {
+    'SERVICE_NAME': ELK_PROJECT,
+    'SERVER_URL': APM_SERVER_URL,
+}
 
 if DEBUG:
     INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
     INSTALLED_APPS = INSTALLED_APPS + ('django_extensions',)
     MIDDLEWARE = MIDDLEWARE + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+else:
+    MIDDLEWARE = MIDDLEWARE + (
+        'elasticapm.contrib.django.middleware.TracingMiddleware',
+        'elasticapm.contrib.django.middleware.Catch404Middleware',
+    )
+    LOGGING['handlers']['logstash'] = {
+        'level': 'DEBUG',
+        'class': 'logstash.TCPLogstashHandler',
+        'host': LOGSTASH_URL,
+        'port': 5959,
+        'version': 1,
+        'message_type': 'logstash',
+        'fqdn': False,
+        'tags': [ELK_PROJECT]
+    }
+    LOGGING['handlers']['elasticapm'] = {
+        'level': 'INFO',
+        'class': 'elasticapm.contrib.django.handlers.LoggingHandler',
+    }
 
 
 ROOT_URLCONF = 'indigo_bylaws.urls'
